7257c00cb9b60bbb659420f77e1f0912cf27e86f
LiuWenlong <liuwenlong@intern.douban.com>
2017-05-20 18:17:51 +0000

    refactor for deployment (at /var/www/fans656.me)
    go visit https://fans656.me/

---
+++ config.py
@@ -0,0 +1,16 @@
+session_duration_days = 7
+sweep_expires_probability = 1.0 / 1000
+
+#db_engine = 'sqlite'
+db_engine = 'mysql'
+
+try:
+    from secret_config import *
+except ImportError:
+    import os
+    print 'Please decrypt the secret_config.py.gpg file'
+    os.system('gpg -o secret_config.py secret_config.py.gpg')
+    try:
+        from secret_config import *
+    except ImportError:
+        print 'Failed to import secret_config'
